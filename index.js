aaf0d6d47ceacd8e59018ab90d504f72f404c5d2
capitalhealthcaree <premierpatientappointment@gmail.com>
2023-04-19 17:09:26 +0500

    get last three blogs

--- index.js
+++ index.js
@@ -114,6 +114,15 @@ app.get("/appointment/getAll", async (req, res) => {
 //     res.status(500).json({ err: "getting some error" });
 //   }
 // });
+// get last three blogs
+app.get("/news/getLastThree", async (req, res) => {
+  try {
+    const data = await Blog.find().sort({ _id: -1 }).limit(3);
+    res.status(200).json({ data });
+  } catch (err) {
+    res.status(500).json({ err: "error getting blogs" });
+  }
+});
 // get all blogs by pagination
 app.get("/blogs/getAll/pagination", async (req, res) => {
   const page = parseInt(req.query.page) || 1; // default to first page if page is not specified
